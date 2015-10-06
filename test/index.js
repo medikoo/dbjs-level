515e652a92b951fb0534eae3dc4850ed9e25f384
Mariusz Nowak <medyk@medikoo.com>
2015-10-06 17:21:41 +0200

    Improve tests

--- test/index.js
+++ test/index.js
@@ -29,6 +29,7 @@ module.exports = function (t, a, d) {
 		}, multiple: true }
 	});
 
+	zzz.delete('bar');
 	driver.trackComputed(db.Object, 'computed');
 	driver.trackComputed(db.Object, 'computedSet');
 	deferred(
@@ -36,6 +37,7 @@ module.exports = function (t, a, d) {
 		driver.storeEvent(bar._lastOwnEvent_),
 		driver.storeEvent(foo._lastOwnEvent_),
 		driver.storeEvent(aaa._lastOwnEvent_),
+		driver.storeEvent(zzz.getDescriptor('bar')._lastOwnEvent_),
 		driver.storeCustom('elo', 'marko')
 	)(function () {
 		return driver.storeEvents([
